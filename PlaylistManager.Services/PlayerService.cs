dcc48a192d66a5692fce49e01e80d7d53ce3d402
Riccardo Ceriani <riccardo.ceriani@itsincom.it>
2023-04-20 20:01:28 +0200

    fix getcurrenttrack

--- PlaylistManager.Services/PlayerService.cs
+++ PlaylistManager.Services/PlayerService.cs
@@ -1,4 +1,5 @@
 ﻿using PlaylistManager.Data.ToPlaylistManager;
+using System.Net;
 using System.Text.Json;
 
 namespace PlaylistManager.Services
@@ -21,6 +22,7 @@ namespace PlaylistManager.Services
             HttpResponseMessage response = _httpClient.GetAsync($"https://api.spotify.com/v1/me/player").Result;
             _httpClient.DefaultRequestHeaders.Remove("Authorization");
             if (!response.IsSuccessStatusCode) throw new Exception(_utils.StatusCode(response));
+            if (response.Content.Headers.ContentLength == 0) throw new Exception("204");
             Data.FromSpotify.Player? player = JsonSerializer.Deserialize<Data.FromSpotify.Player>(response.Content.ReadAsStream());
             Track track = player?.item is not null ? new Track(player.item) : throw new Exception(player is null ? "500" : "404");
             if (player?.context?.type == "playlist") track.IsFromQueue = player.context.uri == $"spotify:playlist:{_playlistService.GetPlaylist(token, "pmqueue").Id}";
